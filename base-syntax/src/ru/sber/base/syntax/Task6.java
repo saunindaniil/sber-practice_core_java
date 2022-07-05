cb85a8693f6c92b6625d790d3ee524a86d9e6c39
saunindaniil <108753954+saunindaniil@users.noreply.github.com>
2022-07-05 20:30:05 +0300

    Delete base-syntax directory

--- base-syntax/src/ru/sber/base/syntax/Task6.java
+++
@@ -1,9 +0,0 @@
-package ru.sber.base.syntax;
-
-public class Task6 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            System.out.println("*****");
-        }
-    }
-}
