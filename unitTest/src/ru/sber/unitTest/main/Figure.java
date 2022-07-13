4fa036103c014f5b7476f4b7d761cd01f5b71a06
Даниил <daniil.saunini@yandex.ru>
2022-07-13 23:59:37 +0300

    Задание по теме "UnitTest"
    # Conflicts:
    #	UnitTest/src/ru/sber/UnitTest/main/ArithmeticOperation.java
    #	UnitTest/src/ru/sber/UnitTest/main/Figure.java
    #	UnitTest/src/ru/sber/UnitTest/main/TestedClass.java
    #	UnitTest/src/ru/sber/UnitTest/test/TestedClassTest.java

---
+++ unitTest/src/ru/sber/unitTest/main/Figure.java
@@ -0,0 +1,31 @@
+package ru.sber.unitTest.main;
+
+public class Figure extends ArithmeticOperation {
+
+    private final static double PI = 3.14;
+
+    public double perimeterSquare(int a) {
+        return multiplication(4,a);
+    }
+
+    public double areaSquare(int a) {
+        return involution(a, 2);
+    }
+
+    public double perimeterRectangle(int a, int b) {
+        return multiplication(summation(a, b), 2);
+    }
+
+    public double areaRectangle(int a, int b) {
+        return multiplication(a, b);
+    }
+
+    public double perimeterCircle(double r) {
+        return multiplication(multiplication(2, PI), r);
+    }
+
+    public double areaCircle(double r) {
+        return multiplication(PI, involution(r, 2));
+    }
+
+}
---
+++ unitTest/src/ru/sber/unitTest/main/Operations.java
@@ -0,0 +1,27 @@
+package ru.sber.unitTest.main;
+
+public class Operations {
+    public double summation(double a, double b) {
+        return a + b;
+    }
+
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+    }
+
+    public double involution(double a, int power) {
+        double result = 1;
+        for (int i = 0; i < power;i++) {
+            result = result * a;
+        }
+        return result;
+    }
+}
