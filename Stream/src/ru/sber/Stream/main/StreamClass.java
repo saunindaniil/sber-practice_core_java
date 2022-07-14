3138d0b0c999210fa5d38337bf5e9d1f15e859a4
saunindaniil <108753954+saunindaniil@users.noreply.github.com>
2022-07-14 21:46:19 +0300

    Delete Stream directory

--- Stream/src/ru/sber/Stream/main/StreamClass.java
+++
@@ -1,9 +0,0 @@
-package ru.sber.Stream.main;
-
-import java.util.Optional;
-
-public class StreamClass {
-    public static StreamClass createInstance(StreamClass streamClass) {
-        return Optional.ofNullable(streamClass).orElseGet(StreamClass::new);
-    }
-}
